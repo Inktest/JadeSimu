f0be632ff3a62b58bc713b2c755cf10e0d0ae558
Ink <40661903+Inktest@users.noreply.github.com>
2024-10-11 19:44:55 +0200

    Update grafcetSimu.js

--- src/simu/grafcetSimu.js
+++ src/simu/grafcetSimu.js
@@ -230,7 +230,7 @@ function convertDiagramToNodes() {
             coord1[1] = Number.parseInt(coord1[1]) + 0.1
             coord2[1] = Number.parseInt(coord2[1]) + 0.1
             let line = new Line(coord1, coord2, 1, DEFAULT_COLOR)
-            line.translate([offsetX/dotSpace/scale, offsetY/dotSpace/scale]).draw()
+            line.translate([offsetX, offsetY]).draw()
         }
 
     }
